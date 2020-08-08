cc64c6e93b384f775d65146793ecfae03253bb27
TVillegas <villegas545@gmail.com>
2020-08-08 11:15:29 -0600

    version 1.0

---
+++ resources/views/vista.php
@@ -0,0 +1,16 @@
+<?php  
+echo $datos['curso1'];
+?>
+
+
+<?php
+foreach($datos as $value):
+?>
+
+<?php
+echo $value;
+?>
+
+<?php 
+endforeach
+?>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('vista', function () {
+    
+    $datos=array(
+        'curso1' => 'Javascript',
+        'curso2' => 'Java',
+        'curso3' => 'PHP'
+    );
+    return view('vista')->with('datos',$datos);
+});
